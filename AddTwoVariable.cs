1f0a041c15c01faeefe0dcaaf1836a365d790131
mehedi61 <emonmehedi61@gmail.com>
2018-02-08 22:35:41 +0600

    added two variable

---
+++ AddTwoVariable.cs
@@ -0,0 +1,17 @@
+// programmed by MD. Mehedi Hasan 
+
+using System;
+
+   class Add_Two_Numbers {
+       
+       static void Main() {
+           
+           int num_1, num_2, sum;	// declaring variables .
+           
+           num_1 = 10;
+           num_2 = 15;
+           sum = num_1 + num_2;		// adding two variables .
+           
+           Console.WriteLine(" Result is " + sum + " .");	// printing the result .
+       }
+   }
