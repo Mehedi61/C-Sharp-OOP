07d910ee0f8e36069d1cbdf67e4fda04f62fe70c
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-02-16 23:54:14 +0600

    printing 1-20 using for loop

---
+++ forLoop.cs
@@ -0,0 +1,21 @@
+// programmed by MD. Mehedi Hasan
+
+using System;
+
+namespace LoopApplication {
+    
+    class For_Loop {
+
+        static void Main() {
+            
+            int n;  // declaring a variable without value
+
+            for (n = 1; n <= 20 ; n++) {
+                
+                // assigned 1 as the value of n and printing 1-20 using loop     
+                Console.WriteLine("Value of N is: {0}", n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
