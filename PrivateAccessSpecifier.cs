94e2da5481d68e5adff64884cb01c71ea324750c
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-02-21 23:59:27 +0600

    encapsulation: private access specifier

---
+++ PrivateAccessSpecifier.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace RectangleApplication {
+    
+    class Rectangle_Area {
+        
+        // member variables
+        private double length;
+        private double width;
+
+        private void Accept_Details () {
+
+            Console.Write("Enter the value of Length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the value of Width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public void Get_Accept_Details() {
+            
+            Accept_Details();
+        }
+
+        public double Get_Area () {
+            return length * width;
+        }
+
+        public void Display () {
+
+            Console.WriteLine();
+            Console.WriteLine("Area: {0}", Get_Area());
+        }
+    } // end of class Rectangle
+    
+    class Print_Rectangle {
+
+        static void Main() {
+            
+            Rectangle_Area r = new Rectangle_Area();
+            r.Get_Accept_Details();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
