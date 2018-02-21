a2d8a9bed34a7536d4366f5adbb2b4185b3b68b4
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-02-21 15:27:59 +0600

    accept input from user

--- acceptingInputfromUser.cs
+++ acceptingInputfromUser.cs
@@ -12,9 +12,11 @@ namespace Input_Accepting_Application {
 
         Console.Write("Enter your name: ");
 
-        user_name = Console.ReadLine();      // getting name from user
+        user_name = Console.ReadLine();      // taking input from user
 
         Console.WriteLine("Your name is {0}.", user_name);
+
+        Console.ReadLine();
 
         }
     }
