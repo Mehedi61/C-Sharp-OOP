28b2281c38735635ab06cf3f5811431316afa14e
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-02 02:57:26 +0600

    use of enum variable

---
+++ enum.cs
@@ -0,0 +1,29 @@
+// Programmed by MD. Mehedi Hasan
+
+using System;
+
+namespace EnumApp {
+
+   class Enum {
+      
+      enum Numbers {zero, one, two, three, four, five};
+
+      static void Main(string[] args) {
+      
+        int zero = (int)Numbers.zero;
+        int one = (int)Numbers.one;
+        int two = (int)Numbers.two;
+        int three = (int)Numbers.three;
+        int four = (int)Numbers.four;
+        int five = (int)Numbers.five;         
+
+        Console.WriteLine("Zero: {0}", zero);
+        Console.WriteLine("One: {0}", one);
+	    Console.WriteLine("Two: {0}", two);
+	    Console.WriteLine("Three: {0}", three);
+	    Console.WriteLine("Four: {0}", four);
+	    Console.WriteLine("Five: {0}", five);
+        Console.ReadLine();
+      }
+   }
+}
